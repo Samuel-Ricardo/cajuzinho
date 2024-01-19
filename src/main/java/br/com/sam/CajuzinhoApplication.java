63e4ea7288de272478447ab501a308c34eda2158
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-19 17:44:29 -0300

    [ :seedling: ] | init: project :D

---
+++ src/main/java/br/com/sam/CajuzinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajuzinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajuzinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sam/CajuzinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajuzinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

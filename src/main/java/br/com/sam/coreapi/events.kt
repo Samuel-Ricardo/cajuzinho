cba5ecdb2d3b428c0f9e76b38497c850b9a588ce
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-19 18:05:30 -0300

    [ :incoming_envelope: ] | create: event - deselected > product (coreapi)

--- src/main/java/br/com/sam/coreapi/events.kt
+++ src/main/java/br/com/sam/coreapi/events.kt
@@ -3,11 +3,17 @@ package br.com.sam.coreapi
 import org.axonframework.commandhandling.RoutingKey
 import java.util.UUID
 
-class FoodCartCreatedEvent(
+data class FoodCartCreatedEvent(
         val foodCartId: UUID
 )
 
 data class ProductSelectedEvent (
+        val foodCartId: UUID,
+        val productId: UUID,
+        val quantity: Int,
+)
+
+data class ProductDeselectedEvent (
         val foodCartId: UUID,
         val productId: UUID,
         val quantity: Int,

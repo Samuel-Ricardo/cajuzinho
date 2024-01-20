c1d9d5c972cdcaca160bee827c94db0c9ed0dbaa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-20 10:17:14 -0300

    [ :card_file_box: ] | create: repository - FoodCartView (repository)

---
+++ src/main/java/br/com/sam/repository/FoodCartViewRepository.kt
@@ -0,0 +1,7 @@
+package br.com.sam.repository
+
+import br.com.sam.query.FoodCartView
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface FoodCartViewRepository: JpaRepository<FoodCartView, UUID> 

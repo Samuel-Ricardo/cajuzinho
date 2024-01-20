355367ba3dee3e064df9a07799962a4530f87a79
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-20 11:02:28 -0300

    [ :traffic_light: ] | create: controller - FoodOrdering > create (ui)

--- src/main/java/br/com/sam/ui/FoodOrderingController.java
+++ src/main/java/br/com/sam/ui/FoodOrderingController.java
@@ -1,9 +1,14 @@
 package br.com.sam.ui;
 
+import br.com.sam.coreapi.CreateFoodCartCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RequestMapping("/foodCart")
 @RestController
@@ -17,7 +22,10 @@ public class FoodOrderingController {
         this.queryGateway = queryGateway;
     }
 
-    
 
+    @PostMapping("/create")
+    public CompletableFuture<UUID> createFoodCart(){
+        return commandGateway.send(new CreateFoodCartCommand(UUID.randomUUID()));
+    }
 
 }

0ad17f151d665f1c307d38a4222e7559b5749431
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-20 10:26:17 -0300

    [ :incoming_envelope: ] | create: FoodCart - created (query::projector:handler)

--- src/main/java/br/com/sam/query/FoodCartProjector.java
+++ src/main/java/br/com/sam/query/FoodCartProjector.java
@@ -1,8 +1,13 @@
 package br.com.sam.query;
 
+import br.com.sam.coreapi.FoodCartCreatedEvent;
+import br.com.sam.coreapi.ProductSelectedEvent;
 import br.com.sam.repository.FoodCartViewRepository;
+import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Component
 public class FoodCartProjector {
@@ -12,5 +17,10 @@ public class FoodCartProjector {
     public FoodCartProjector(FoodCartViewRepository repository) {
         this.repository = repository;
     }
-    
+
+    @EventHandler
+    public void on(FoodCartCreatedEvent event) {
+        var view = new FoodCartView(event.getFoodCartId(), Collections.emptyMap());
+        repository.save(view);
+    }
 }

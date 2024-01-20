fbbabec8beb206b0b69f6db2377104d47e44dcf2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-19 22:44:56 -0300

    [ :gun: ] | create: command - select > product (coreapi)

--- src/main/java/br/com/sam/coreapi/commands.kt
+++ src/main/java/br/com/sam/coreapi/commands.kt
@@ -1,9 +1,17 @@
 package br.com.sam.coreapi
 
 import org.axonframework.commandhandling.RoutingKey
+import org.axonframework.modelling.command.TargetAggregateIdentifier
 import java.util.UUID
 
 
 data class CreateFoodCartCommand (
     @RoutingKey val foodCartId: UUID
+)
+
+data class SelectProductCommand (
+    @TargetAggregateIdentifier
+    val foodCartId: UUID,
+    val productId: UUID,
+    val quantity: Int,
 )

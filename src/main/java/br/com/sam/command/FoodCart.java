bcfd4a9da6fb543088456f006b043cd16903d1fa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-19 23:04:54 -0300

    [ :sparkles: ] | create: handler - FoodCart > create (command)

--- src/main/java/br/com/sam/command/FoodCart.java
+++ src/main/java/br/com/sam/command/FoodCart.java
@@ -1,6 +1,10 @@
 package br.com.sam.command;
 
+import br.com.sam.coreapi.CreateFoodCartCommand;
+import br.com.sam.coreapi.FoodCartCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,8 @@ import java.util.UUID;
 @Aggregate
 public class FoodCart {
 
+    FoodCart(){}
+
     private static final Logger logger = LoggerFactory.getLogger(FoodCart.class);
 
     @AggregateIdentifier
@@ -18,6 +24,10 @@ public class FoodCart {
     private Map<UUID, Integer> selectedProducts;
     private boolean confirmed;
 
-    
+
+    @CommandHandler
+    public FoodCart(CreateFoodCartCommand  command) {
+        AggregateLifecycle.apply(new FoodCartCreatedEvent(command.getFoodCartId()));
+    }
 
 }
